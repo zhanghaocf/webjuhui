55291251d72715e8211075070e777ddad173b29f
groot <zhaicf37@163.com>
2019-02-19 17:33:13 +0800

    mock axios

---
+++ js/mock.js
@@ -0,0 +1,17 @@
+data:{
+	//轮播图
+	list: [
+		{
+			id:1,
+			img:"./src/banner.png",
+			url:'./detail.html?img=./src/link/detail1.jpg'
+		},
+		{
+			id:2,
+			img:"./src/banner.png",
+			url:'./detail.html?img=./src/link/detail1.jpg'
+		}					
+	],
+	}
+
+}
